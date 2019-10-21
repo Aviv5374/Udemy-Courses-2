585bbd49395df730cfd3efeac460d4f8db1c3d85
Aviv5374 <aviv5374@gmail.com>
2019-10-21 15:14:27 +0300

    Use Animation Events

---
+++ 3D Cours/Zombie Runner 2/Assets/_MyAssets/Scritps/Enemy/EnemyAttack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField] float damage = 40f;
+    [SerializeField] Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void AttackHitEvent()
+    {
+        if (target == null) return;
+        Debug.Log("bang bang");
+    }
+
+}
