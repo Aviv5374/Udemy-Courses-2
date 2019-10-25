3b8237640a9858d07507f0b6d6090b48d9fd212f
Aviv5374 <Aviv5374@gmail.com>
2019-10-25 19:50:32 +0300

    My Version Of Set Active Weapon &  Player Input To Select Weapon 2

--- 3D Cours/Zombie Runner 2/Assets/_MyAssets/Scritps/Weapon/WeaponSwitcher.cs
+++ 3D Cours/Zombie Runner 2/Assets/_MyAssets/Scritps/Weapon/WeaponSwitcher.cs
@@ -4,29 +4,62 @@ using UnityEngine;
 
 public class WeaponSwitcher : MonoBehaviour
 {
+    int currentWeaponIndex = 0;
+
     //TODO: make it work with a list, like Borderlands.
     //the player start with 1 weapon and when she level up more weapon slots are open.
     Weapon[] weapons;
+
+    int CurrentWeaponIndex 
+    { 
+        get => currentWeaponIndex;
+        set 
+        {
+            currentWeaponIndex = value;
+
+            if (currentWeaponIndex > weapons.Length -1)
+            {
+                currentWeaponIndex = 0;
+            }
+
+            if (currentWeaponIndex < 0)
+            {
+                currentWeaponIndex = weapons.Length - 1;
+            }
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         weapons = GetComponentsInChildren<Weapon>();
-        ResetWeaponActivity();
-        weapons[0].gameObject.SetActive(true);
+        SetWeaponActive(CurrentWeaponIndex);
     }
 
     // Update is called once per frame
     void Update()
     {
-        SwitchWeapon();
+        SwitchWeapon();        
     }
 
     void SwitchWeapon()
-    {
-        //TODO: add the option to switch weapons whit Mouse ScrollWheel
-
+    {        
+        ProcessScrollWheel();
         ProcessKeyInput();
+        SetWeaponActive(CurrentWeaponIndex);
+    }
+
+    void ProcessScrollWheel()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            CurrentWeaponIndex++;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            CurrentWeaponIndex--;
+        }
     }
 
     void ProcessKeyInput()
@@ -35,13 +68,18 @@ public class WeaponSwitcher : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Alpha1 + index))
             {
-                ResetWeaponActivity();
-                weapons[index].gameObject.SetActive(true);
+                CurrentWeaponIndex = index;               
             }
         }
     }
 
-    private void ResetWeaponActivity()
+    void SetWeaponActive(int index)
+    {
+        ResetWeaponActivity();
+        weapons[index].gameObject.SetActive(true);
+    }
+
+    void ResetWeaponActivity()
     {
         for (int index = 0; index < weapons.Length; index++)
         {
