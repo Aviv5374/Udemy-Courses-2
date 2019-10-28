d97a66e1166e855d43d63cf35e39dbcf769b2196
Aviv5374 <Aviv5374@gmail.com>
2019-10-28 15:23:40 +0200

    Ammo Pickup - Part 1

---
+++ 3D Cours/Zombie Runner 2/Assets/_MyAssets/Scritps/Ammo/AmmoPickup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Ammo ammo = other.GetComponent<Ammo>();
+        if (ammo)
+        {
+            Debug.Log("Player did what players do");
+            Destroy(gameObject);
+        }
+    }
+}
