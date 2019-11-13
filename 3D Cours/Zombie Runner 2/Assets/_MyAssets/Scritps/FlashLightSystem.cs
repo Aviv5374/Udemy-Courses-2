afcc5644b8e470f659c939d22cb5f839e0f3174e
Aviv5374 <Aviv5374@gmail.com>
2019-11-13 16:21:03 +0200

    Create A FlashLight

---
+++ 3D Cours/Zombie Runner 2/Assets/_MyAssets/Scritps/FlashLightSystem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class FlashLightSystem : MonoBehaviour
+{
+    [SerializeField] float lightDecay = .1f;
+    [SerializeField] float angleDecay = 1f;
+    [SerializeField] float minimumAngle = 40f;
+
+    Light myLight;   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+
+    void DecreaseLightAngle()
+    {
+        if (myLight.spotAngle > minimumAngle)
+        {
+            myLight.spotAngle -= angleDecay * Time.deltaTime;
+        }
+    }
+
+    void DecreaseLightIntensity()
+    {
+        if (myLight.intensity > 0)
+        {
+            myLight.intensity -= lightDecay * Time.deltaTime;
+        }
+    }
+}
